061232d5eced3066cfd671358ea2baed288ede79
KeriMaeva <Karinchikmaeva@bk.ru>
2023-02-24 16:42:03 +0300

    Добавили 2 задачу в 1 домашней работе с Проверкой истинности утверждения

--- HomeWork_01/Task_2.py
+++ HomeWork_01/Task_2.py
@@ -1,5 +1,17 @@
 # 2*. Напишите программу для
-# проверки истинности утверждения 
-# ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z 
+# проверки истинности утверждения
+# ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z
 # для всех значений предикат.
 
+flag = True
+for X in (0, 1):
+    for Y in (0, 1):
+        for Z in (0, 1):
+            f = not (X or Y or Z) == (not X and not Y and not Z)
+            if f == False:
+                flag = False
+            print(X, Y, Z, f)
+if flag:
+    print('Доказано')
+else:
+    print('Нет, не всех случаях')
